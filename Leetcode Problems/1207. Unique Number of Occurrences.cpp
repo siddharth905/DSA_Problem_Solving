f095d590944d66cc1c8983c365a4ee24dd730945
siddharth905 <121485094+siddharth905@users.noreply.github.com>
2024-01-17 22:21:39 +0530

    Create 1207. Unique Number of Occurrences

---
+++ Leetcode Problems/1207. Unique Number of Occurrences.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int ,int>map;
+        set<int>s;
+        for(int i=0;i<arr.size();i++){
+            map[arr[i]]++;
+        }
+        for(auto it=map.begin();it != map.end();it++){
+            int frqn=it->second;
+            if(s.find(frqn) != s.end()){
+                return false;
+            }
+            else{
+                  s.insert(frqn);
+            }
+        }
+        return true;
+        // if(map.size() == s.size()){
+        //     return true;
+        // }
+       // return false;
+    }
+};
+/*
+   Time complexity=O(N);  //Time complexity of find() function??
+   Space Complexity=O(N)
+*/
